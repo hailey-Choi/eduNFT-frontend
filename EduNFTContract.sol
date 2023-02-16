bc61d2f2c39302c8b17ecbabda589d2c25ce5fe5
7ongxia <7ongxia@gmail.com>
2023-02-16 22:15:57 +0800

    working on purchase

--- EduNFTContract.sol
+++ EduNFTContract.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.6;
+pragma experimental ABIEncoderV2;
 
 /**
  * @dev Interface of the KIP-13 standard, as defined in the
@@ -1076,7 +1077,7 @@ contract KIP17Metadata is KIP13, KIP17, IKIP17Metadata {
     string private _symbol;
 
     // Optional mapping for token URIs
-    mapping(uint256 => string) private _tokenURIs;
+    mapping(uint256 => string) internal _tokenURIs;
 
     /*
      *     bytes4(keccak256('name()')) == 0x06fdde03
@@ -1563,8 +1564,9 @@ contract EduNFT is KIP17Full, Ownable {
     Counters.Counter private _itemsSold;
 
     //The structure to store info about a listed token
-    struct ListedToken {
+    struct MintedToken {
         uint256 tokenId;
+        string tokenURI;
         address owner;
         address seller;
         uint256 price;
@@ -1574,6 +1576,7 @@ contract EduNFT is KIP17Full, Ownable {
     //the event emitted when a token is successfully listed
     event TokenListedSuccess (
         uint256 indexed tokenId,
+        string tokenURI,
         address owner,
         address seller,
         uint256 price,
@@ -1581,96 +1584,207 @@ contract EduNFT is KIP17Full, Ownable {
     );
 
     //This mapping maps tokenId to token info and is helpful when retrieving details about a tokenId
-    mapping(uint256 => ListedToken) private idToListedToken;
+    mapping(uint256 => MintedToken) private idToMintedToken;
 
     constructor() public KIP17Full("EduNFT", "EduNFT") {}
 
+    //This will return all the NFTs
+    function getAllNFTs() public view returns (MintedToken[] memory) {
+        uint nftCount = _tokenIds.current();
+        MintedToken[] memory tokens = new MintedToken[](nftCount);
+        uint currentIndex = 0;
+
+        //at the moment currentlyListed is true for all, if it becomes false in the future we will 
+        //filter out currentlyListed == false over here
+        for(uint i=0; i<nftCount; i++)
+        {
+            uint currentId = i + 1;
+            MintedToken storage currentItem = idToMintedToken[currentId];
+            tokens[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+        //the array 'tokens' has the list of all NFTs in the marketplace
+        return tokens;
+    }
+
+    //This will return all the listed NFTs
+    function getAllListedNFTs() public view returns (MintedToken[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+        
+        //Important to get a count of all the NFTs that belong to the user before we can make an array for them
+        for(uint i=0; i < totalItemCount; i++)
+        {
+            if(idToMintedToken[i+1].currentlyListed == true){
+                itemCount += 1;
+            }
+        }
+
+        //Once you have the count of relevant NFTs, create an array then store all the NFTs in it
+        MintedToken[] memory items = new MintedToken[](itemCount);
+        for(uint i=0; i < totalItemCount; i++) {
+            if(idToMintedToken[i+1].currentlyListed == true) {
+                uint currentId = i+1;
+                MintedToken storage currentItem = idToMintedToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    //Returns all the NFTs that the current user is owner or seller in
+    function getMyNFTs() public view returns (MintedToken[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+        
+        //Important to get a count of all the NFTs that belong to the user before we can make an array for them
+        for(uint i=0; i < totalItemCount; i++)
+        {
+            if(idToMintedToken[i+1].owner == msg.sender || idToMintedToken[i+1].seller == msg.sender){
+                itemCount += 1;
+            }
+        }
+
+        //Once you have the count of relevant NFTs, create an array then store all the NFTs in it
+        MintedToken[] memory items = new MintedToken[](itemCount);
+        for(uint i=0; i < totalItemCount; i++) {
+            if(idToMintedToken[i+1].owner == msg.sender || idToMintedToken[i+1].seller == msg.sender) {
+                uint currentId = i+1;
+                MintedToken storage currentItem = idToMintedToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    //Returns all the listed NFTs that the current user is owner or seller in
+    function getMyListedNFTs() public view returns (MintedToken[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+        
+        //Important to get a count of all the NFTs that belong to the user before we can make an array for them
+        for(uint i=0; i < totalItemCount; i++)
+        {
+            if(idToMintedToken[i+1].currentlyListed == true){
+                if(idToMintedToken[i+1].owner == msg.sender || idToMintedToken[i+1].seller == msg.sender){
+                    itemCount += 1;
+                }
+            }
+        }
+
+        //Once you have the count of relevant NFTs, create an array then store all the NFTs in it
+        MintedToken[] memory items = new MintedToken[](itemCount);
+        for(uint i=0; i < totalItemCount; i++) {
+            if(idToMintedToken[i+1].currentlyListed == true) {
+                if(idToMintedToken[i+1].owner == msg.sender || idToMintedToken[i+1].seller == msg.sender){
+                    uint currentId = i+1;
+                    MintedToken storage currentItem = idToMintedToken[currentId];
+                    items[currentIndex] = currentItem;
+                    currentIndex += 1;
+                }
+            }
+        }
+        return items;
+    }
+
+    function getLatestNFT() public view returns (MintedToken memory) {
+        uint256 currentTokenId = _tokenIds.current();
+        return idToMintedToken[currentTokenId];
+    }
+
+    function getMintedNFT(uint256 tokenId) public view returns (MintedToken memory) {
+        return idToMintedToken[tokenId];
+    }
+
+    function getCurrentTokenId() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
     function mintNFT(string memory tokenURI)
         public
-        onlyOwner
-        returns (uint256)
-    {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
-    }
-
-    function listedTokens(uint256 tokenId) 
-        public
-        view
-        returns (
-            uint256,
-            address,
-            address,
-            uint256,
-            bool
-        )
-    {
-        return (
-            idToListedToken[tokenId].tokenId, 
-            idToListedToken[tokenId].owner, 
-            idToListedToken[tokenId].seller, 
-            idToListedToken[tokenId].price, 
-            idToListedToken[tokenId].currentlyListed
-        );
-    }
-
-
-    function listNFT(string memory tokenURI, uint256 price)
-        public
-        payable
-        returns (uint256)
+        returns (MintedToken memory)
     {
         //Increment the tokenId counter, which is keeping track of the number of minted NFTs
         _tokenIds.increment();
-        uint256 newTokenId = _tokenIds.current();
+        uint256 newItemId = _tokenIds.current();
 
         //Mint the NFT with tokenId (newTokenId) to the address who called listNFT
-        _mint(msg.sender, newTokenId);
+        _mint(msg.sender, newItemId);
         //Map the tokenId to the tokenURI (which is an IPFS URL with the NFT metadata)
-        _setTokenURI(newTokenId, tokenURI);
+        _setTokenURI(newItemId, tokenURI);
 
-        //Helper function to update Global variables and emit an event
-        createListedToken(newTokenId, price);
+        idToMintedToken[newItemId] = MintedToken(
+            newItemId,
+            _tokenURIs[newItemId],
+            msg.sender,
+            msg.sender,
+            0,
+            false
+        );
 
-        return newTokenId;
+        return idToMintedToken[newItemId];
     }
 
-    function createListedToken(uint256 tokenId, uint256 price) 
-        private
+    function listNFT(uint256 tokenId, uint256 price) 
+        public
     {
-        // Make sure the sender sent enough Klay to pay for listing
-        // require(msg.value == listPrice, "Hopefully sending the correct price");
         //Just sanity check
         require(price > 0, "Make sure the price isn't negative");
+
+        address tokenOwner = idToMintedToken[tokenId].owner;
+        require(tokenOwner == msg.sender, "caller is not token owner");
 
         //Update the mapping of tokenId's to Token details, useful for retrieval functions
         // address payable ownner = address(uint256(address(this)));
         // address payable seller = msg.sender;
 
-        idToListedToken[tokenId] = ListedToken(
+        idToMintedToken[tokenId] = MintedToken(
             tokenId,
+            _tokenURIs[tokenId],
             address(this),
             msg.sender,
             price,
             true
         );
-
-        listedTokens(tokenId);
 
         _transferFrom(msg.sender, address(this), tokenId);
         //Emit the event for successful transfer. The frontend parses this message and updates the end user
         emit TokenListedSuccess(
             tokenId,
+            _tokenURIs[tokenId],
             address(this),
             msg.sender,
             price,
             true
         );
+    }
+
+    function purchaseNFT(uint256 tokenId) public payable {
+        uint price = idToMintedToken[tokenId].price;
+        address seller = idToMintedToken[tokenId].seller;
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        //update the details of the token
+        idToMintedToken[tokenId].seller = msg.sender;
+        idToMintedToken[tokenId].owner = msg.sender;
+        idToMintedToken[tokenId].price = 0;
+        idToMintedToken[tokenId].currentlyListed = false;
+        _itemsSold.increment();
+
+        //approve the marketplace to sell NFTs on your behalf
+        approve(address(this), tokenId);
+        //Actually transfer the token to the new owner
+        _transferFrom(address(this), msg.sender, tokenId);
+
+        //Transfer the listing fee to the marketplace creator
+        payable(owner).transfer(listPrice);
+        //Transfer the proceeds from the sale to the seller of the NFT
+        payable(seller).transfer(msg.value);
     }
 
 }
