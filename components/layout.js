349103112c8335814a73e914bfab395ae45c5b92
Hailey Choi <tngus254@hku.hk>
2023-04-01 17:30:57 +0800

    fix import issue and add homepage ui

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,8 @@ import Caver from "caver-js";
 import { Button } from "./Button";
 import { Container } from "./Container";
 import { Modal } from "./modal";
+import Link from "next/link";
+
 export default function Layout({ children }) {
   const [connected, setConnected] = useState(false);
   const [address, setAddress] = useState(null);
@@ -60,12 +62,18 @@ export default function Layout({ children }) {
       <header className="relative z-50 pb-11 lg:pt-11">
         <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
           <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
-            <h1
-              id="speakers-title"
-              className="font-bold text-5xl tracking-tighter  sm:text-5xl"
+            <Link
+              href={{
+                pathname: "/",
+              }}
             >
-              EduNFT
-            </h1>
+              <h1
+                id="speakers-title"
+                className="font-bold text-5xl tracking-tighter  sm:text-5xl"
+              >
+                EduNFT
+              </h1>
+            </Link>
           </div>
           <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
             <div className="mx-auto flex items-center gap-4 px-4">
--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,88 @@ import { Button } from "../components/Button";
 import Layout from "../components/layout";
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [textInput, setTextInput] = useState("");
   return (
     <Layout home>
       <Container>
-        <div>
-          <div>
-            <label
-              htmlFor="textinput"
-              className="block text-lg font-medium text-gray-700"
-            >
-              Enter a keyword
-            </label>
-            <div className="mt-3 mb-3">
-              <input
-                type="text"
-                id="textInput"
-                name="textInput"
-                value={textInput}
-                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 lg:text-lg"
-                placeholder="white cat"
-                onChange={(e) => {
-                  setTextInput(e.target.value);
-                }}
-              />
+        <div className="mt-20">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mx-60">
+              The Ultimate Educational Platform for AI and NFT
+            </h1>
+            <p className="mt-10 mx-20 text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt.
+              Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat
+              veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non
+              deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
+              amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna
+              aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+              commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua. Anim
+              aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+              cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
+              aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui
+              irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+              occaecat fugiat aliqua.
+            </p>
+            <div className="inline-flex gap-40 mt-10">
+              <Image src="/angry_elfo.jpeg" alt="me" width="300" height="400" />
+              <Image src="/angry_elfo.jpeg" alt="me" width="300" height="400" />
             </div>
+            <p className="mt-10 mx-20 text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt.
+              Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat
+              veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non
+              deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
+              amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna
+              aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+              commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua. Anim
+              aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+              cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
+              aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui
+              irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+              occaecat fugiat aliqua.
+            </p>
           </div>
-          <Link
-            href={{
-              pathname: "/dall-e",
-              query: {
-                keyword: textInput,
-              },
-            }}
-          >
-            <Button> Generate Images</Button>
-          </Link>
+          <div className="flex flex-col items-center  text-center mt-20 mb-24">
+            <div>
+              <label
+                htmlFor="textinput"
+                className="block text-lg font-medium text-gray-700"
+              >
+                Enter a keyword
+              </label>
+              <div className="mt-3 mb-3 w-96">
+                <input
+                  type="text"
+                  id="textInput"
+                  name="textInput"
+                  value={textInput}
+                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 lg:text-lg text-center"
+                  placeholder="white cat"
+                  onChange={(e) => {
+                    setTextInput(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <Link
+              href={{
+                pathname: "/dall-e",
+                query: {
+                  keyword: textInput,
+                },
+              }}
+            >
+              <Button className="p-4 text-s mt-2 ml-2"> Generate Images</Button>
+            </Link>
+          </div>
         </div>
       </Container>
     </Layout>
